d9f5b44cf870873055ac525972d06e1fe58c671f
Farah <farah.hsh.12@gmail.com>
2023-08-14 18:28:21 +0300

    day7

---
+++ main.js
@@ -0,0 +1,28 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// day7  
+
+//  interaction with user
+// dailog boxes  conatian:  alert, prompt, confirm       we will take alert and prompt
+
+// alert    syntax: alert(message)
+
+// alert("hello world");
+// alert(2*2);
+
+// prompt    
+// let names= prompt("what is your name?",);
+// let age= prompt("hello " + names + " how old are you");
+
+// end
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+// challenge
+let names2= prompt("what is your name?",);
+let age2= prompt("hello " + names2 + " how old are you");
+alert( "your name is "+ names2 + " your age is " + age2 +" years old ");
+// end of challenge
+
+
+
